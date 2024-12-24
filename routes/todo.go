e89cf3c08afb56ae033a5449aec1d3cd7b2ea9d8
Furkan Ateş <furkanates4@posta.mu.edu.tr>
2024-12-24 16:23:01 +0300

    feat: add update todo handler route

--- routes/todo.go
+++ routes/todo.go
@@ -108,10 +108,69 @@ func GetTodosHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(todos)
 }
 
+func UpdateTodoHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPut {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	userID, ok := r.Context().Value(middlewares.UserIDKey).(uuid.UUID)
+	if !ok {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	todoIDStr := r.URL.Path[len("/todo/update/"):]
+	todoID, err := uuid.Parse(todoIDStr)
+	if err != nil {
+		http.Error(w, "Invalid Todo ID", http.StatusBadRequest)
+		return
+	}
+
+	var requestBody struct {
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	todo, err := controllers.GetTodo(todoID)
+	if err != nil {
+		if err.Error() == "todo not found" {
+			http.Error(w, "Todo not found", http.StatusNotFound)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if todo.UserID != userID {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	todo.Title = requestBody.Title
+	todo.Description = requestBody.Description
+
+	updatedTodo, err := controllers.UpdateTodo(todo)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedTodo)
+}
+
 func TodoRoutes() {
 	http.Handle("/todo/create", middlewares.AuthMiddleware(http.HandlerFunc(CreateTodoHandler)))
 
 	http.Handle("/todo/get/", middlewares.AuthMiddleware(http.HandlerFunc(GetTodoHandler)))
 
 	http.Handle("/todo/get", middlewares.AuthMiddleware(http.HandlerFunc(GetTodosHandler)))
+
+	http.Handle("/todo/update/", middlewares.AuthMiddleware(http.HandlerFunc(UpdateTodoHandler)))
 }
